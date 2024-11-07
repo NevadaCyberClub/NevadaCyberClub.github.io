f9f50a54c19739616c94e3aa7691681498c22cfa
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2024-11-07 13:57:05 -0800

    add mobile support for doanteio links

--- js/donate_redirect.js
+++ js/donate_redirect.js
@@ -1,1 +1,1 @@
-location.replace("https://securelb.imodules.com/s/1949/lg21/form.aspx?sid=1949&gid=2&pgid=667&cid=1890&bledit=1&dids=3873")
+location.replace("https://unr.imodules.com/s/1949/bp21/crowdfunding.aspx?sid=1949&gid=2&pgid=2682")
